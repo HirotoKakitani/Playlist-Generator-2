50ba43ca1d8024aa63e44fd8a4200e1b448c62ab
Hiroto Kakitani <hikakitani@hds.ad.ucsd.edu>
2018-08-16 07:57:49 -0700

    buttons functional

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,20 @@
-/*let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', function(data) {
-	changeColor.style.backgroundColor = data.color;
-	changeColor.setAttribute('value', data.color);
+var inputTerm = null;
+var playlistURL = "https://www.youtube.com/embed/";
+var first = true;
+var idList = [];
+$(document).ready(function(){
+	console.log("document loaded");
+	$("#add").click(function(){
+		console.log("Add button pressed");
+		chrome.tabs.query({'active': true, 'currentWindow': true},function(tabs){ //look into lastFocusedWindow
+			//TODO parse tabs[0].url for video id, then push to idList
+			idList.push(tabs[0].url);	//push current url to list
+			console.log(idList);
+		});
+		
+	});
+	$("#play").click(function(){
+		console.log("play button pressed");
+	});
 });
 
-changeColor.onClick = function(element){
-	let color = element.taget.value;
-	chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
-	chrome.tabs.executeScript(
-		tabs[0].id,
-		{code: 'document.body.style.backgroundColor = "' + color + '";'});
-	});
-};*/
