225ff0974501903020cce615f991d0e936eda306
Hiroto Kakitani <hikakitani@hds.ad.ucsd.edu>
2018-08-17 16:10:58 -0700

    Can create playlist URLs with 1 video

--- js/background.js
+++ js/background.js
@@ -1,11 +1,8 @@
 chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.set({color: '#3aa757'}, function() {
-		console.log("Installed correctly");
-	});
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {hostEquals:'www.google.com', schemes: ['https']},
+				pageUrl: {hostEquals:'www.youtube.com', schemes: ['https']},
 			})
 			],
 				actions: [new chrome.declarativeContent.ShowPageAction()]
--- js/popup.js
+++ js/popup.js
@@ -2,19 +2,46 @@ var inputTerm = null;
 var playlistURL = "https://www.youtube.com/embed/";
 var first = true;
 var idList = [];
+var vidID = null;
+//TODO  chrome storage
+/*
+	1. when add is pressed, store the vidID in chrome storage area.
+	2. when play is pressed, get every ID in chrome storage area and generate playlist URL
+*/
 $(document).ready(function(){
+	//chrome.storage.sync.set();
 	console.log("document loaded");
 	$("#add").click(function(){
 		console.log("Add button pressed");
 		chrome.tabs.query({'active': true, 'currentWindow': true},function(tabs){ //look into lastFocusedWindow
-			//TODO parse tabs[0].url for video id, then push to idList
-			idList.push(tabs[0].url);	//push current url to list
+			//parse tabs[0].url for video id, then push to idList
+			vidID = tabs[0].url.split("https://www.youtube.com/watch?v=").pop();
+			
+			//check if the videoID was correctly parsed
+			if (vidID == tabs[0].url){
+				console.log("Cannot add this video");
+			};
+			idList.push(vidID);	//push current url to list
+			console.log(tabs[0].url);
+			console.log(vidID);
 			console.log(idList);
+			
 		});
-		
 	});
 	$("#play").click(function(){
 		console.log("play button pressed");
+		$.each(idList, function(i, item){
+			if (first){
+				playlistURL = playlistURL.concat(item);
+				playlistURL = playlistURL.concat("?playlist=");
+				first = false;
+			}
+			else{
+				playlistURL = playlistURL.concat(item);
+				playlistURL = playlistURL.concat(",");
+			}
+		});
+		console.log(playlistURL);
+		
 	});
 });
-
